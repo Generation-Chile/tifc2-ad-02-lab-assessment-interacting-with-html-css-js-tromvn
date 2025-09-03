a03ef3fb44e14c050fe045f8b6ef4d64876c10fa
tromvn <tomas.bazan@genstudents.org>
2025-09-03 16:30:00 -0400

    Tarea entregada

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.getElementById("first").innerHTML = "GoodBye"
+
+document.getElementById("orange").style.fontSize = "large"
+
+function changeFontColour() {
+    document.getElementById("toBrown").style.color = "Brown"
+}
